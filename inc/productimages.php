eb6f0329670737dfb49d0871e45546587d3ab63c
AMAN1312993 <amanraj662@yahoo.com>
2021-05-06 23:55:53 +0530

    first commit

---
+++ inc/productimages.php
@@ -0,0 +1,20 @@
+<?php
+header("content-Type: application/json");
+
+if (isset ($_GET["product"])) {
+  $data = array( );
+   $productName=str_replace(" ","+",$ GET['product']);
+
+   $productLink ="http://placehold.it/1024x768?text=".$productName;
+
+   for($i=0; $i<3; $i++){
+
+      $data[] =$productLink."+".($i+1);
+
+   }
+   echo json_encode($data);
+
+}
+else{
+  echo json_encode("http://placehold.it/1024x768?text=error+no+product");
+}
---
+++ signin.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Shop Home</title>
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!-- font-awesome -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+  <!-- Header -->
+  <header>
+    <div class="fluid-container">
+      <div class="jumbotron text-center">
+        <h1>Shop Home🛒</h1>
+        <p>Offering wide Ranges of Products from daily basic uses to Electronics Accessories</p>
+      </div>
+    </div>
+
+  </header>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="fluid-container">
+      <header class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="index.html" class="navbar-brand">Shop Home</a>
+      </header>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Deals</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="signin/sign.php"><span class="glyphicon glyphicon-user"></span>Sign In</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
+        <li>
+          <form class="navbar-form" action="index.html" method="post">
+            <input type="text" class="form-control" placeholder="Serach For a Products">
+            <button type="submit" name="button" class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
+
+          </form>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
+</nav>
+  <!-- Main -->
+  <main>
+    <div class="container">
+  </main>
+  <!-- Footer -->
+  <footer class="text-center">
+  <nav class="navbar navbar-inverse navbar-bottom">
+      <div class="fluid-container">
+        <ul class="nav navbar-nav">
+          <li class="navbar-text">Copyright &copy; Aman Raj</li>
+          <li><a href="www.facebook.com"><i class="fa fa-facebook">Facebook</i></a></li>
+          <li><a href="#"><i class="fa fa-google">Google</i></a></li>
+          <li><a href="#"><i class="fa fa-twitter">Twitter</i></a></li>
+          <li><a href="#"><i class="fa fa-linkedin">Linkdin</i></a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Payments Methods</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Site Map</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-communication"></span>Contact Us</a></li>
+          <li><a href="#"> Terms &amp; conditions</a></li>
+        </ul>
+
+        <p>Test</p>
+      </div>
+
+    </nav>
+  </footer>
+  <!-- Footer -->
+
+
+  <!-- Javascript -->
+  <!-- Jquery -->
+
+  <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script> -->
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+  <!-- customjs -->
+</body>
+
+</html>
